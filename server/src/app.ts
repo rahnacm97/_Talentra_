44fd82a84b73a5be37c2977f352f14826dd397e7
Rahna <rahnacm97@gmail.com>
2026-01-29 21:31:13 +0530

    env backend url live

--- server/src/app.ts
+++ server/src/app.ts
@@ -50,7 +50,7 @@ SocketManager.initialize(io, [chatHandler, videoCallHandler]);
 app.use(cookieParser());
 app.use(
   cors({
-    origin: [process.env.FRONTEND_URL!, "http://localhost:5173"],
+    origin: [process.env.FRONTEND_URL!, "http://localhost:5173","https://talentra.site"],
 
     credentials: true,
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
@@ -60,6 +60,7 @@ app.use(
 app.use(express.json());
 
 connectDB();
+
 
 // Routes
 app.use("/api/auth", authRoutes);

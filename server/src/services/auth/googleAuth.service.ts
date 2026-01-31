8ad67c3a3a0157ce96697bb2f5739c4733352605
Rahna <rahnacm97@gmail.com>
2026-01-31 10:46:26 +0530

    Google authentication fixes

--- server/src/services/auth/googleAuth.service.ts
+++ server/src/services/auth/googleAuth.service.ts
@@ -17,7 +17,7 @@ export class GoogleAuthService {
         {
           clientID: process.env.GOOGLE_CLIENT_ID!,
           clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-          callbackURL: `${process.env.BACKEND_URL}/api/auth/google/callback`,
+          callbackURL: `${process.env.BACKEND_URL}/auth/google/callback`,
           passReqToCallback: true,
         },
         async (req, accessToken, refreshToken, profile, done) => {
